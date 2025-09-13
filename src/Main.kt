5a8845e82cbb73fd0b4c1bcec3a7a464a6fc289e
Роман Зорин <sgena170315@gmail.com>
2025-09-13 16:27:05 +0300

    Задание 6. Сумма чисел

--- src/Main.kt
+++ src/Main.kt
@@ -41,5 +41,14 @@ fun main() {
     println("$num1 - $num2 = ${num1 - num2}")
     println("$num1 * $num2 = ${num1 * num2}")
 
+    //Task 6
+    print("Введите трехзначное число: ")
+    val number = readln().toInt()
+    val digit1 = number / 100
+    val digit2 = (number % 100) / 10
+    val digit3 = number % 10
+    val sum = digit1 + digit2 + digit3
+    println("$digit1 + $digit2 + $digit3 = $sum")
+
 }
     
