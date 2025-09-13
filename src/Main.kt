98253d37f0512845032b33ac25c5ea403aebec7b
Роман Зорин <sgena170315@gmail.com>
2025-09-13 16:15:16 +0300

    Задание 1. Переменные

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    //Task 1
+    var fav_game = "ARK: Survival Evolved";
+    var fav_d = 7;
+    val PI = 3.14159;
+    var fav_l = 'Z';
+
+    println("$fav_game , $fav_d , $PI, $fav_l");
+}
+    
